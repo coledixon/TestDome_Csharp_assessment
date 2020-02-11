f4ae14bc968134de46d50f95f35fc0ec51c9de40
colefromportland <colefromportland@gmail.com>
2020-02-11 12:52:32 -0700

    add test params for both drills

---
+++ CSharp_online_skillTest/CSharp_online_skillTest/DRILL 1/drill1_params.cs
@@ -0,0 +1,19 @@
+﻿// DRILL PARAMETERS (4 test cases)
+
+// BASE CODE PROVIDED
+using System;
+
+public class MergeNames
+{
+    public static string[] UniqueNames(string[] names1, string[] names2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+        string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+        Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
