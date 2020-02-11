8ad630230f6597555072c028a5c6713d0f3a2f3d
colefromportland <colefromportland@gmail.com>
2020-02-11 12:52:53 -0700

    built solutions based on test params and default code base

---
+++ CSharp_online_skillTest/CSharp_online_skillTest/DRILL 1/drill1_solution.cs
@@ -0,0 +1,34 @@
+﻿// SOLUTION // CD 2020
+// PASSED 3 of 4 test cases (failed on unique names array)
+using System;
+using System.Collections.Generic;
+
+public class MergeNames
+{
+    public static string[] UniqueNames(string[] names1, string[] names2)
+    {
+        // string[] result = new string[names1.Length];
+
+        List<string> result = new List<string>();
+
+        foreach (string n1 in names1)
+        {
+            result.Add(n1);
+        }
+        foreach (string n2 in names2)
+        {
+            if (!result.Contains(n2))
+            {
+                result.Add(n2);
+            }
+        }
+        return result.ToArray();
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+        string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+        Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
---
+++ CSharp_online_skillTest/CSharp_online_skillTest/DRILL 2/drill2_solution.cs
@@ -0,0 +1,48 @@
+﻿// SOLUTION // CD 2020
+// PASSED 3 of 3 test cases
+using System.Collections.Generic;
+using System;
+
+public class AlertService
+{
+    private readonly AlertDAO storage = new AlertDAO();
+    public IAlertDAO _iad;
+
+    public AlertService(IAlertDAO iad)
+    {
+        _iad = iad;
+    }
+
+    public Guid RaiseAlert()
+    {
+        return _iad.AddAlert(DateTime.Now);
+    }
+
+    public DateTime GetAlertTime(Guid id)
+    {
+        return _iad.GetAlert(id);
+    }
+}
+
+public class AlertDAO : IAlertDAO
+{
+    private readonly Dictionary<Guid, DateTime> alerts = new Dictionary<Guid, DateTime>();
+
+    public Guid AddAlert(DateTime time)
+    {
+        Guid id = Guid.NewGuid();
+        this.alerts.Add(id, time);
+        return id;
+    }
+
+    public DateTime GetAlert(Guid id)
+    {
+        return this.alerts[id];
+    }
+}
+
+public interface IAlertDAO
+{
+    Guid AddAlert(DateTime time);
+    DateTime GetAlert(Guid id);
+}
