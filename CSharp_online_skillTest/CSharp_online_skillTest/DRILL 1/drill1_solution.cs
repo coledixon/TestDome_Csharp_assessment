b29768d1750666bd102caba19b7084a043379ad4
colefromportland <colefromportland@gmail.com>
2020-02-12 08:37:23 -0700

    added file to project namespace accessibility && scope

--- CSharp_online_skillTest/CSharp_online_skillTest/DRILL 1/drill1_solution.cs
+++ CSharp_online_skillTest/CSharp_online_skillTest/DRILL 1/drill1_solution.cs
@@ -3,30 +3,34 @@
 using System;
 using System.Collections.Generic;
 
-public class MergeNames
+namespace CSharp_online_skillTest.DRILL_1
 {
-    public static string[] UniqueNames(string[] names1, string[] names2)
+    public class MergeNames
     {
-        List<string> result = new List<string>();
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            List<string> result = new List<string>();
 
-        foreach (string n1 in names1)
-        {
-            result.Add(n1);
-        }
-        foreach (string n2 in names2)
-        {
-            if (!result.Contains(n2))
+            foreach (string n1 in names1)
             {
-                result.Add(n2);
+                result.Add(n1);
             }
+            foreach (string n2 in names2)
+            {
+                if (!result.Contains(n2))
+                {
+                    result.Add(n2);
+                }
+            }
+            return result.ToArray();
         }
-        return result.ToArray();
-    }
 
-    //public static void Main(string[] args)
-    //{
-    //    string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
-    //    string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
-    //    Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
-    //}
+        // CD: moved fuctionality to Program.cs
+        //public static void Main(string[] args)
+        //{
+        //    string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+        //    string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+        //    Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+        //}
+    }
 }
