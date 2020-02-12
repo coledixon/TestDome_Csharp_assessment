439fc014f1822fb064905062d8c752e5dcb4db59
colefromportland <colefromportland@gmail.com>
2020-02-12 08:37:53 -0700

    commented out params due to compile issues

--- CSharp_online_skillTest/CSharp_online_skillTest/DRILL 2/drill2_params.cs
+++ CSharp_online_skillTest/CSharp_online_skillTest/DRILL 2/drill2_params.cs
@@ -9,37 +9,37 @@
 
 
 // BASE CODE PROVIDED
-using System.Collections.Generic;
-using System;
+//using System.Collections.Generic;
+//using System;
 
-public class AlertService
-{
-    private readonly AlertDAO storage = new AlertDAO();
+//public class AlertService
+//{
+//    private readonly AlertDAO storage = new AlertDAO();
 
-    public Guid RaiseAlert()
-    {
-        return this.storage.AddAlert(DateTime.Now);
-    }
+//    public Guid RaiseAlert()
+//    {
+//        return this.storage.AddAlert(DateTime.Now);
+//    }
 
-    public DateTime GetAlertTime(Guid id)
-    {
-        return this.storage.GetAlert(id);
-    }
-}
+//    public DateTime GetAlertTime(Guid id)
+//    {
+//        return this.storage.GetAlert(id);
+//    }
+//}
 
-public class AlertDAO
-{
-    private readonly Dictionary<Guid, DateTime> alerts = new Dictionary<Guid, DateTime>();
+//public class AlertDAO
+//{
+//    private readonly Dictionary<Guid, DateTime> alerts = new Dictionary<Guid, DateTime>();
 
-    public Guid AddAlert(DateTime time)
-    {
-        Guid id = Guid.NewGuid();
-        this.alerts.Add(id, time);
-        return id;
-    }
+//    public Guid AddAlert(DateTime time)
+//    {
+//        Guid id = Guid.NewGuid();
+//        this.alerts.Add(id, time);
+//        return id;
+//    }
 
-    public DateTime GetAlert(Guid id)
-    {
-        return this.alerts[id];
-    }
-}
+//    public DateTime GetAlert(Guid id)
+//    {
+//        return this.alerts[id];
+//    }
+//}
