6ff4f348f7cad3438c41e48da65f4e6c4cd8345d
colefromportland <colefromportland@gmail.com>
2020-02-18 14:33:34 -0700

    configuring all test scripts to run out of Program.cs based on user input

--- CSharp_online_skillTest/CSharp_online_skillTest/Program.cs
+++ CSharp_online_skillTest/CSharp_online_skillTest/Program.cs
@@ -1,4 +1,8 @@
+using CSharp_online_skillTest.DRILL_1;
+using CSharp_online_skillTest.DRILL_2;
+using CSharp_online_skillTest.DRILL_3;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +10,90 @@ using System.Threading.Tasks;
 
 namespace CSharp_online_skillTest
 {
-    class Program
+    public class drill_validation
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            int input;
+
+            select_drill:
+            Console.WriteLine("SELECT C# DRILL TO VALIDATE");
+            Console.WriteLine("select a drill using 1, 2, or 3. || or type 0 to exit program");
+            bool pass = int.TryParse(Console.ReadLine(), out input);
+
+            if (!pass)
+            {
+                Console.WriteLine("please enter a numeric value");
+                goto select_drill;
+            }
+            else
+            {
+                if (input == 0) { return; }
+                else if (input > 3 || input < 0)
+                {
+                    Console.WriteLine("please limit your numeric entry to 1, 2, or 3");
+                    goto select_drill;
+                }
+            }
+
+            switch (input)
+            {
+                case 1:
+                    Execute_Drill1();
+                    break;
+                case 2:
+                    Execute_Drill2();
+                    break;
+                case 3:
+                    Execute_Drill3();
+                    break;
+            }
+        }
+
+        public static void Execute_Drill1()
+        {
+            Console.WriteLine("DRILL 1 : unique name method");
+            Console.WriteLine("---- press  enter to continue ----");
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+            Console.ReadKey();
+
+            Main(null);
+        }
+
+        public static void Execute_Drill2()
+        {
+            Console.WriteLine("DRILL 2 : interface / constructor");
+            Console.WriteLine("this a strictly programming drill, no output for user to evaluate");
+            Console.WriteLine("---- press enter to continue ----");
+            Console.ReadKey();
+
+            Main(null);
+        }
+
+        public static void Execute_Drill3()
+        {
+            bool _res;
+
+            Console.WriteLine("DRILL 3 : anagram string compare");
+            Console.WriteLine("---- press enter to continue ----");
+            Console.ReadKey();
+            // debug
+            _res = AnagramEval.EvalAnagram("Bendy", "beNDs"); // false
+            Console.WriteLine("evlauation 1 (bendy : bends) = "+_res);
+            Console.ReadKey();
+            _res = AnagramEval.EvalAnagram("angel", "angle"); // true
+            Console.WriteLine("evlauation 2 (angel : angle) = " + _res);
+            Console.ReadKey();
+            _res = AnagramEval.EvalAnagram("listen", "silent"); // true
+            Console.WriteLine("evlauation 3 = (listen : silent) " + _res);
+            Console.ReadKey();
+
+            Main(null);
         }
     }
 }
